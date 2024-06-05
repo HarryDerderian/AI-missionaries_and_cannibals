75f38dc3496b8b3c2938b3b29c155e95f86817fb
HarryDerderian <harryderderian@csu.fullerton.edu>
2024-06-05 12:01:44 -0700

    code refactor

--- misscannibals.py
+++ misscannibals.py
@@ -6,11 +6,10 @@ class MissCannibals(Problem):
         self.M = M
         self.C = C
         self.possible_actions = ['MM', 'MC', 'CC', 'M', 'C']
-
         super().__init__(initial, goal)
 
     def result(self, state, action):
-        x = -1 if state[2] else 1 
+        x = -1 if state[2] else 1 # Sets the direction for unit movement.
         return (state[0] + action.count('M')*x, state[1] + action.count('C')*x, not state[2])
 
     def actions(self, state):
@@ -20,19 +19,19 @@ class MissCannibals(Problem):
             # Confirm Missionaries are in range
             if test_state[0] < 0 or test_state[0] > self.M :
                 valid_actions.remove(action)
-            # Confirm Cannibals  are in range    
+            # Confirm Cannibals are in range    
             elif test_state[1] < 0 or test_state[1] > self.C : 
                 valid_actions.remove(action)
-            # Confirm Missionaries  aren't outnumbered on left bank
+            # Confirm Missionaries aren't outnumbered on left bank
             elif test_state[0] > 0 and test_state[0] < test_state[1] : 
                 valid_actions.remove(action)
-            # Confirm Missionaries  aren't outnumbered on right bank
+            # Confirm Missionaries aren't outnumbered on right bank
             elif self.M - test_state[0] > 0 and (self.M - test_state[0]) < (self.C - test_state[1]) :
                 valid_actions.remove(action)
         return valid_actions
     
 if __name__ == '__main__':
-    mc = MissCannibals(M=3, C=3)
+    mc = MissCannibals(M=4, C=4)
     path = depth_first_graph_search(mc).solution()
     print(path)
     path = breadth_first_graph_search(mc).solution()
